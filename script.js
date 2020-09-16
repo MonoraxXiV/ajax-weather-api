6351b5ed50ae787ada145e8f16688be393ca2202
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-16 15:37:02 +0200

    able to obtain data from fetch

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-
-
-
+const WeatherKey = config.WEATHERK;
 document.getElementById("run").addEventListener("click", function () {
- getInput();
 
-
+    getWeatherData();
 
 })
 
-//need to be careful due to the limit on data pulls.
-function getInput (){
-    let input = document.getElementById('CityName').value;
-    console.log(input);
-}
-
 function getWeatherData() {
-// still figuring out which one to use there fetch()
+     let input = document.getElementById('CityName').value;
+     console.log(input);
+     document.getElementById('cityDisplay').innerHTML = input;
 
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' +input + '&units=metric&appid=' +WeatherKey)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
 }
+
