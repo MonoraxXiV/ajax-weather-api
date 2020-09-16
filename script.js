8d29ee734340959fef63606c35a9ced47d170c35
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-16 12:31:49 +0200

    very basics rn, reading through api, figuring out which link is correct

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+document.getElementById("run").addEventListener("click", function () {
+ getInput();
+
+
+
+})
+
+//need to be careful due to the limit on data pulls.
+function getInput (){
+    let input = document.getElementById('CityName').value;
+    console.log(input);
+}
+
+function getWeatherData() {
+// still figuring out which one to use there fetch()
+
+}
