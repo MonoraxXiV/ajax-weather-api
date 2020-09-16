45da3d3b0b0466e5c8376b5539a52e3557716a85
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-16 16:44:15 +0200

    managed to pull one temperature, added notes on displaying

--- script.js
+++ script.js
@@ -6,14 +6,22 @@ document.getElementById("run").addEventListener("click", function () {
 })
 
 function getWeatherData() {
-     let input = document.getElementById('CityName').value;
-     console.log(input);
-     document.getElementById('cityDisplay').innerHTML = input;
+    let input = document.getElementById('CityName').value;
+    console.log(input);
 
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' +input + '&units=metric&appid=' +WeatherKey)
+
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + input + '&units=metric&appid=' + WeatherKey)
         .then(response => response.json())
         .then(data => {
             console.log(data)
+            document.getElementById('cityDisplay').innerHTML = data.city.name; //shows correct city in text
+
+            let temp1 = data.list[0].main.temp; //gets data from today at 3pm
+            console.log(temp1)
+            //need to find out how to get the data of each day, dt_txt shows the date and time.
+            //each day has several time slots divided by every three hours.
+            //shows in celcius at least.
         })
+
 }
 
